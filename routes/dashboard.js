11a0f6421c3ea3f3601ada105fffe3d4f968511b
Tolga Kara <tolgakara2.0@gmail.com>
2020-09-16 09:16:18 +0200

    Added Signup Login and Dashboard

---
+++ routes/dashboard.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/dashboard", (req, res, next) => {
+	let completedBooks = { name: "ExampleNameCompleted", path: "ExamplePathCompleted" };
+	let currentBooks = { name: "ExampleNameCurrent", path: "ExamplePathCurrent" };
+	let wishListBooks = { name: "ExampleNameWishList", path: "ExamplePathWishList" };
+	let completedLength = 0;
+	let currentLength = 0;
+	let wishListLength = 0;
+
+	res.render("./dashboard/dashboard", {
+		completedBooks,
+		currentBooks,
+		wishListBooks,
+		completedLength,
+		currentLength,
+		wishListLength,
+	});
+});
+
+module.exports = router;
