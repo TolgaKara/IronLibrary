0ac60f5b83a7420663fea10c3f7cefe94f57694e
Tolga Kara <tolgakara2.0@gmail.com>
2020-09-17 12:16:35 +0200

    Deployment Ready Setup

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ const logger = require("morgan");
 const path = require("path");
 
 mongoose
-	.connect("mongodb://localhost/ironlibrary", { useNewUrlParser: true, useUnifiedTopology: true })
+	.connect(process.env.MONGODB_URI || "mongodb://localhost/ironlibrary", {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
 	.then((x) => {
 		console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
 	})
